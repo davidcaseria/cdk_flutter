cbea088e7df805450c239345a05a61622daf0974
David Caseria <david@team.sovereign.app>
2025-02-17 11:38:44 -0500

    Export mint and token types

--- lib/cdk_flutter.dart
+++ lib/cdk_flutter.dart
@@ -3,6 +3,8 @@ library;
 import 'package:cdk_flutter/src/rust/frb_generated.dart';
 
 export 'src/rust/api/error.dart';
+export 'src/rust/api/mint.dart';
+export 'src/rust/api/token.dart';
 export 'src/rust/api/wallet.dart';
 export 'widgets.dart';
 
